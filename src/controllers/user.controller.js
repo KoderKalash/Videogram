0e670c880379b8ca1f848724c766ecf40dbb8dc6
Koder Kalash <kalashmashup@gmail.com>
2025-12-06 16:32:40 +0530

    fix: added register controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler"
 import { apiError } from "../utils/apiError"
 import { User } from "../models/user.model"
+import { cloudinary } from "../utils/cloudinary"
+import { apiResponse } from "../utils/apiResponse"
+
+
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     const { fullname, email, password } = req.body
@@ -18,23 +22,50 @@ const registerUser = asyncHandler(async (req, res) => {
     const existedUser = User.findOne({
         $or: [{ username }, { email }]
     })
-    if(existedUser){
-        throw new apiError(409,"User with email or username already exists")
+    if (existedUser) {
+        throw new apiError(409, "User with email or username already exists")
     }
-    
+
     // check for images & avatar
     const avatarLocalPath = req.files?.avatar[0]?.path
     const coverImagePath = req.files?.coverImage[0]?.path
-    if(!avatarLocalPath){
-        throw new apiError(400,"Aatar file is required!")
+    if (!avatarLocalPath) {
+        throw new apiError(400, "Aatar file is required!")
     }
-    
+
     // upload them to cloudinary, avatar
+    const avatar = await cloudinary.uploader.upload(avatarLocalPath)
+    const coverImage = await cloudinary.uploader.upload(coverImagePath)
+
+    if (!avatar) {
+        throw new apiError(400, "Avatar file is required!")
+    }
+
     // create user object - create entry in db
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage.url,
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
     // remove password and referesh token field from response
-    // check for user creation 
+    const createdUser = await User.findById(user._id).select(
+        "-password -refereshToken" //apart from these two give every detail of user
+    )
+
+    // check for user creation
+    if (!createdUser) {
+        throw new apiError(500, "Something went wrong while creating the user.")
+    }
+
     // return response if created
-    // else retur
+    return res.status(201).json(
+        new apiResponse(200, "User registered succesfully!")
+    )
+
 })
 
 export { registerUser }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,7 +6,7 @@ import { v2 as cloudinary } from 'cloudinary';
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+        api_secret: process.env.CLOUDINARY_API_SECRET 
     });
     
     // Upload an image
@@ -40,3 +40,5 @@ import { v2 as cloudinary } from 'cloudinary';
     
     console.log(autoCropUrl);    
 })();
+
+export { cloudinary };
